3d691c7ce1e3387472eed0a7467c60c42d7af824
Your Name <donata1989@gmail.com>
2022-04-07 21:38:25 +0200

    Add theme for app

--- client/src/theme/theme.ts
+++ client/src/theme/theme.ts
@@ -1,11 +1,46 @@
-/**
- * Add styles to the theme.
- */
 export const theme = {
   colors: {
-    primary: "#FF9900",
-    secondary: "#FF3D00",
-    highlighted: "#00FF0A",
-    pageBackground: "#eeffff",
+    common: {
+      textColor: "#000",
+      textInactive: "#A59E9E",
+    },
+    navigationBar: {
+      primary: "#73A942",
+      secondary: "#D7FFA6", 
+    },
+    card: {
+      primary: "#F8F3EF",
+      secondary: "#F9F8F8",
+      highlighted: "#EEE6E6",
+      boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)",
+    },
+    button: {
+      primary: "#538D22",
+      secondary: "#F9F8F8",
+      textInactive: "#8B8B8B", 
+    },
+  },
+
+};
+
+export const text = {
+  size: {
+      mobile: {
+        xs: 10,
+        sm: 14,
+        lg: 20,
+        xl: 26,
+      },
+      desktop: {
+        xs: 14,
+        sm: 18,
+        lg: 26,
+        xl: 32,
+      },
+    },
+  weight: {
+    light: 200,
+    regular: 400,
+    bold: 700,
   },
 };
