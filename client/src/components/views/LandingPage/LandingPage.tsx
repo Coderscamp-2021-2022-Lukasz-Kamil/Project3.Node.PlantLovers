dd5a7cb1f97f80d13244b1af47063e377c770fb4
Rafał <xbl@gazeta.pl>
2022-04-08 11:09:19 +0200

    add instruction how to add navigationbar to routes & delete exaple button and wrapper

--- client/src/components/views/LandingPage/LandingPage.tsx
+++ client/src/components/views/LandingPage/LandingPage.tsx
@@ -3,9 +3,13 @@ import React from "react";
 const LandingPage = () => {
   return (
     <div>
-      Jeszcze gdy chodziłem do podstawówki, to był tam taki Paweł, i ja jechałem
-      na rowerze, i go spotkałem, i potem jeszcze pojechałem do biedronki na
-      lody, i po drodze do domu wtedy jeszcze, już do domu pojechałem.
+      <p>LandingPage</p>
+      <p>
+        Jeszcze gdy chodziłem do podstawówki, to był tam taki Paweł, i ja
+        jechałem na rowerze, i go spotkałem, i potem jeszcze pojechałem do
+        biedronki na lody, i po drodze do domu wtedy jeszcze, już do domu
+        pojechałem.
+      </p>
     </div>
   );
 };
--- client/src/components/views/MainView/App.tsx
+++ client/src/components/views/MainView/App.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from "../../../global-styles/GlobalStyles";
 import { theme } from "../../../theme/theme";
-import { Button, ButtonWithBorders } from "../../ui/Button/Button.style";
-import { FlexCenter } from "../../wrappers/FlexCenter/FlexCenter.style";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AddOfferPage from "../AddOfferPage/AddOfferPage";
 import PlantsPage from "../PlantsPage/PlantsPage";
@@ -22,11 +20,6 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <FlexCenter direction="column">
-        <h1>Hello World</h1>
-        <Button>Primary</Button>
-        <ButtonWithBorders>Mam style z Buttona</ButtonWithBorders>
-      </FlexCenter>
       <Router>
         <Routes>
           <Route path="/" element={<LandingPage />} />
@@ -51,6 +44,30 @@ function App() {
       </Router>
     </ThemeProvider>
   );
+
+  /*
+  // Here should be outlent when navbar will be created, like://
+    function PagesWithNavigationBar() {
+      return (
+        <> 
+        //component imported from file with navbar//
+          <NavigationBar />
+          <Outlet />
+        </>
+      );
+    }
+
+    //Aslo navbar must be included in routes e.g.: //
+      <Route path="/" element={<PagesWithNavigationBar />} >
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/add-offer" element={<AddOfferPage />} />
+        <Route path="/plants" element={<PlantsPage />} />
+        <Route path="/user" element={<UserPage />} />
+        <Route path="/user/login" element={<LoginPage />} />
+        <Route path="/user/sign-up" element={<SignUpPage />} />
+        <Route path="/user/your-offers" element={<YourOfferPage />} />
+      </Route>
+  */
 }
 
 export default App;
