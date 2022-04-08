8f952f9ac6c49694cbcf1e742f4a535e63ea50df
Ewelina Mężyk <ewelinamezykmail@gmail.com>
2022-04-08 19:35:32 +0100

    Add props to grid layout

--- client/src/components/wrappers/FlexCenter/GridContainer.style.tsx
+++ client/src/components/wrappers/FlexCenter/GridContainer.style.tsx
@@ -1,14 +1,23 @@
 import styled from "styled-components";
 
-export const GridContainer = styled.div`
+interface GridColumnProps {
+  bigScreenColumns?: number;
+  smallScreenColumns?: number;
+}
+
+export const GridContainer = styled.div<GridColumnProps>`
   display: grid;
-  grid-template-columns: repeat(4, 1fr);
+  grid-template-columns: ${({ bigScreenColumns }) =>
+    bigScreenColumns ? `repeat(${bigScreenColumns}, 1fr)` : `repeat(4, 1fr)`};
   grid-template-rows: auto;
   grid-auto-flow: dense;
   grid-gap: 1em;
   justify-content: center;
 
   @media (max-width: 576px) {
-    grid-template-columns: repeat(2, 1fr);
+    grid-template-columns: ${({ smallScreenColumns }) =>
+      smallScreenColumns
+        ? `repeat(${smallScreenColumns}, 1fr)`
+        : `repeat(2, 1fr)`};
   }
 `;
