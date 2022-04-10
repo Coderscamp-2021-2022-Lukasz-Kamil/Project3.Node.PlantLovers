71997b09facf4a1aca352de88faa776abbd64a58
Your Name <donata1989@gmail.com>
2022-04-10 16:53:56 +0200

    Add font import

--- client/src/global-styles/GlobalStyles.tsx
+++ client/src/global-styles/GlobalStyles.tsx
@@ -1,13 +1,15 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;600;700&display=swap');
+
 *, *:before, *:after{
    box-sizing: border-box;
    padding: 0;
    margin: 0;
 }
 body {
-   font-family: 'Poppisn', sans-serif;
+   font-family: 'Poppins', sans-serif;
    -webkit-font-smoothing: antialiased;
    -moz-osx-font-smoothing: grayscale;
    background-color: ${({ theme }) => theme.colors.common.backgroundcolor};
