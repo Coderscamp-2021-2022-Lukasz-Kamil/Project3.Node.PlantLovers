ea8f8ff1c1e386b7bb1a96b982a7f81f24c5b2c7
Your Name <donata1989@gmail.com>
2022-04-10 18:17:23 +0200

    Move font import into index.html file

--- client/src/global-styles/GlobalStyles.tsx
+++ client/src/global-styles/GlobalStyles.tsx
@@ -1,8 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;600;700&display=swap');
-
 *, *:before, *:after{
    box-sizing: border-box;
    padding: 0;
