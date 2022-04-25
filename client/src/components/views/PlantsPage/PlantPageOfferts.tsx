7f5f722aaa3125654173df80dfb65ef61f6bdb49
Ewelina Mężyk <ewelinamezykmail@gmail.com>
2022-04-25 10:40:22 +0100

    Display all offers

--- client/src/components/views/PlantsPage/PlantPageOfferts.tsx
+++ client/src/components/views/PlantsPage/PlantPageOfferts.tsx
@@ -6,6 +6,9 @@ import { FlexWrapper } from "../../wrappers/FlexCenter/FlexWrapper.style";
 import Icons from "../ProfilePage/YourOfferPage/Icons";
 import { ReactComponent as RandomImage } from "../../../assets/examplePlantPhoto.svg";
 import { GridContainer } from "../../wrappers/FlexCenter/GridContainer.style";
+import useFetchData from "../../../hooks/UseFetch";
+import { Offer } from "../ProfilePage/YourOfferPage/OfferModel";
+import { ImageCard } from "../../ui/ImageCard.style";
 
 const OfferCard = styled(Card)`
   height: 36vh;
@@ -42,90 +45,40 @@ const ImageContainer = styled.div`
   height: 86%;
 `;
 
-//Temporary Array//
 export const PlantPageOfferts = () => {
-  const cards = [
-    {
-      id: "EFFF3Eessdf",
-      name: "Chwast",
-      city: "Żarów",
-      price: 32,
-      picture: <CardImage />,
+  const { response } = useFetchData({
+    url: `/offers`,
+    method: "GET",
+    headers: {
+      accept: "*/*",
+      "Content-Type": "application/json",
     },
-    {
-      id: "EFFgdssdEessdf",
-      name: "Mandragora",
-      city: "Żarów",
-      price: 32,
-      picture: <CardImage />,
-    },
-    {
-      id: "EFFgdss",
-      name: "ARG",
-      city: "Żarów",
-      price: 32,
-      picture: <CardImage />,
-    },
-    {
-      id: "EdsfFFgdss",
-      name: "GFZfg",
-      city: "Żarów",
-      price: 32,
-      picture: <CardImage />,
-    },
-    {
-      id: "ESGEgg",
-      name: "Chwast",
-      city: "Żarów",
-      price: 32,
-      picture: <CardImage />,
-    },
-    {
-      id: "ESGEg5g",
-      name: "Chwast",
-      city: "Żarów",
-      price: 32,
-      picture: <CardImage />,
-    },
-    {
-      id: "ESGgfEgg",
-      name: "Chwast",
-      city: "Żarów",
-      price: 32,
-      picture: <CardImage />,
-    },
-    {
-      id: "ESGEsfdgg",
-      name: "Chwast",
-      city: "Żarów",
-      price: 32,
-      picture: <CardImage />,
-    },
-    {
-      id: "ESGEafggg",
-      name: "Chwast",
-      city: "Żarów",
-      price: 32,
-      picture: <CardImage />,
-    },
-  ];
+  });
+
+  const offers: Offer[] = response;
+
   return (
     <YourOfferGridContainer smallScreenColumns={2}>
-      {cards.map((card) => (
-        <OfferCard width="auto" color="offer" key={card.id}>
-          <ImageContainer>{card.picture}</ImageContainer>
+      {offers.map((offer) => (
+        <OfferCard width="auto" color="offer" key={offer._id}>
+          {/* <ImageContainer>{offer.photos[0].url}</ImageContainer> */}
+          <ImageCard
+            alt="plantPhoto"
+            src={offer.photos[0].url}
+            height="auto"
+          ></ImageCard>
           <FlexWrapper justifyContent="space-between">
             <div>
               <Typography fontSize="xxs" fontSizeMobile="xs">
-                {card.name}
+                {offer.title}
               </Typography>
               <Typography fontSize="xxxs" fontSizeMobile="xs">
-                {card.city}
+                {offer.city}
               </Typography>
             </div>
             <div>
               <PriceTypography fontSize="xxs" fontSizeMobile="xs">
-                {card.price}$
+                {offer.price}$
               </PriceTypography>
               <Icons />
             </div>
