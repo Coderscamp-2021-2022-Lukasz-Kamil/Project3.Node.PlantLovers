91358f829c4cadbbc36f2014bf1e3c8da3c2feb4
Your Name <donata1989@gmail.com>
2022-05-23 18:21:14 +0200

    Add user change data logic

--- client/src/components/views/ProfilePage/ProfilePage/UserData.tsx
+++ client/src/components/views/ProfilePage/ProfilePage/UserData.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 import styled from "styled-components";
 import { Button } from "../../../ui/Button/Button.style";
-import { Input } from "../../../ui/Input/Input.style";
+import { InputUser } from "../../../ui/Input/Input.style";
 import { Typography } from "../../../ui/Typography/Typography.style";
 import { FlexWrapper } from "../../../wrappers/FlexCenter/FlexWrapper.style";
+import { editProfileSchema } from "../../../../shared/inputHandlers/EditProfileInputHandler";
+import { useCookies } from "react-cookie";
+import { useFormik } from "formik";
+import axios from "axios";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { BASE_URL } from "../../../../hooks/UseFetch";
+import { ErrorCard } from "../LoginPage";
+import { useParams } from "react-router-dom";
 
 export const ProfilePageContainerColumn = styled(FlexWrapper)`
   width: 100%;
@@ -12,7 +21,8 @@ export const ProfilePageContainerColumn = styled(FlexWrapper)`
   gap: 0;
 `;
 export const ProfilePageContainerColumnItem = styled.div`
-  width: 70%;
+  font-size: 24px;
+  width: 85%;
   margin-bottom: 30px;
 
   @media (max-width: 576px) {
@@ -23,24 +33,106 @@ export const ProfilePageContainerColumnItem = styled.div`
 export const ProfilePageTypography = styled(Typography)`
   margin-bottom: 10px;
 `;
+
+export const InputUserData = styled(InputUser)`
+  margin-top: 15px;
+`;
+
 const UserData = () => {
-  const allData = [
-    { name: "Name" },
-    { name: "E-mail" },
-    { name: "Phone Number" },
-    { name: "City" },
-  ];
+  const [userId] = useCookies(["user-id"]);
+  const [token] = useCookies(["token"]);
+
+  const editProfileFormHandler = useFormik({
+    initialValues: {
+      email: "",
+      phone: "",
+      city: "",
+    },
+    validationSchema: editProfileSchema,
+    onSubmit: (values) => alert(JSON.stringify(values)),
+  });
+
+  const onEditProfileHandler = async (
+    event: React.FormEvent<HTMLFormElement>
+  ) => {
+    event.preventDefault();
+    editProfileFormHandler.handleSubmit;
+
+    await axios({
+      method: "PUT",
+      url: BASE_URL + `/users/${userId["user-id"]}`,
+      headers: {
+        accept: "*/*",
+        "Content-Type": "application/json",
+        token: token["token"],
+      },
+      data: {
+        email: editProfileFormHandler.values.email || undefined,
+        phoneNumber: editProfileFormHandler.values.phone || undefined,
+        city: editProfileFormHandler.values.city || undefined,
+      },
+    })
+      .then((response) => {
+        return toast.success("Data changed!");
+      })
+      .catch((err) => {
+        if (err.response.data) {
+          return toast.error(`${err.response.data}`);
+        }
+        return toast.error("Incorrect data, check your credentials");
+      });
+  };
+
   return (
     <ProfilePageContainerColumn>
-      {allData.map((data) => (
-        <ProfilePageContainerColumnItem key={data.name}>
-          <ProfilePageTypography fontSize="mdl">
-            {data.name}
-          </ProfilePageTypography>
-          <Input fontSizeDesktop="md" width="100%" />
+      <form onSubmit={onEditProfileHandler}>
+        <ProfilePageContainerColumnItem>
+          E-mail
+          <InputUserData
+            fontSizeDesktop="md"
+            width="100%"
+            type="email"
+            id="email"
+            value={editProfileFormHandler.values.email}
+            onChange={editProfileFormHandler.handleChange}
+            onBlur={editProfileFormHandler.handleBlur}
+          />
+          {editProfileFormHandler.errors.email ? (
+            <ErrorCard>{editProfileFormHandler.errors.email}</ErrorCard>
+          ) : null}
         </ProfilePageContainerColumnItem>
-      ))}
-      <Button>Save changes</Button>
+        <ProfilePageContainerColumnItem>
+          Phone Number
+          <InputUserData
+            fontSizeDesktop="md"
+            width="100%"
+            type="text"
+            id="phone"
+            value={editProfileFormHandler.values.phone}
+            onChange={editProfileFormHandler.handleChange}
+            onBlur={editProfileFormHandler.handleBlur}
+          />
+          {editProfileFormHandler.errors.phone ? (
+            <ErrorCard>{editProfileFormHandler.errors.phone}</ErrorCard>
+          ) : null}
+        </ProfilePageContainerColumnItem>
+        <ProfilePageContainerColumnItem>
+          City
+          <InputUserData
+            fontSizeDesktop="md"
+            width="100%"
+            type="text"
+            id="city"
+            value={editProfileFormHandler.values.city}
+            onChange={editProfileFormHandler.handleChange}
+            onBlur={editProfileFormHandler.handleBlur}
+          />
+          {editProfileFormHandler.errors.city ? (
+            <ErrorCard>{editProfileFormHandler.errors.city}</ErrorCard>
+          ) : null}
+        </ProfilePageContainerColumnItem>
+        <Button type="submit">Save changes</Button>
+      </form>
     </ProfilePageContainerColumn>
   );
 };
--- client/src/components/views/ProfilePage/ProfilePage/UserPassword.tsx
+++ client/src/components/views/ProfilePage/ProfilePage/UserPassword.tsx
@@ -2,28 +2,164 @@ import React from "react";
 import { Button } from "../../../ui/Button/Button.style";
 import { Input } from "../../../ui/Input/Input.style";
 import {
+  InputUserData,
   ProfilePageContainerColumn,
   ProfilePageContainerColumnItem,
   ProfilePageTypography,
 } from "./UserData";
 
+import { useNavigate } from "react-router";
+import { useCookies } from "react-cookie";
+import { useFormik } from "formik";
+import axios from "axios";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { BASE_URL } from "../../../../hooks/UseFetch";
+import { ErrorCard } from "../LoginPage";
+import { useParams } from "react-router-dom";
+import { editCredentialsSchema } from "../../../../shared/inputHandlers/EditCredentialsInputHandler";
+
 const UserPassword = () => {
-  const allData = [
-    { name: "Current Password" },
-    { name: "New Password" },
-    { name: "Confirm New Password" },
-  ];
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [userId, setId, removeUserId] = useCookies(["user-id"]);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [token, setToken, removeToken] = useCookies(["token"]);
+
+  const editCredentialsFormHandler = useFormik({
+    initialValues: {
+      currentPassword: "",
+      newPassword: "",
+      confirmPassword: "",
+    },
+    validationSchema: editCredentialsSchema,
+    onSubmit: (values) => alert(JSON.stringify(values)),
+  });
+
+  const navigate = useNavigate();
+  const navigateTo = () => {
+    navigate("/user/login");
+  };
+
+  const onEditCredentialsHandler = async (
+    event: React.FormEvent<HTMLFormElement>
+  ) => {
+    event.preventDefault();
+    editCredentialsFormHandler.handleSubmit;
+
+    await axios({
+      method: "PUT",
+      url: BASE_URL + `/users/${userId["user-id"]}`,
+      headers: {
+        accept: "*/*",
+        "Content-Type": "application/json",
+        token: token["token"],
+      },
+      data: {
+        currentPassword: editCredentialsFormHandler.values.currentPassword,
+        newPassword: editCredentialsFormHandler.values.newPassword,
+        confirmPassword: editCredentialsFormHandler.values.confirmPassword,
+      },
+    })
+      .then((response) => {
+        removeUserId("user-id");
+        removeToken("token");
+        navigateTo();
+        return toast.success("Password changed!");
+      })
+      .catch(() => {
+        return toast.error("Incorrect data, check your credentials");
+      });
+  };
+
+  const noFieldsAreTouched = () => {
+    return (
+      !editCredentialsFormHandler.touched.currentPassword &&
+      !editCredentialsFormHandler.touched.newPassword &&
+      !editCredentialsFormHandler.touched.confirmPassword
+    );
+  };
+
+  const allFieldsAreTouched = () => {
+    return (
+      editCredentialsFormHandler.touched.currentPassword &&
+      editCredentialsFormHandler.touched.newPassword &&
+      editCredentialsFormHandler.touched.confirmPassword
+    );
+  };
+
+  const anyFieldInputIsInvalid = () => {
+    return (
+      !!editCredentialsFormHandler.errors.currentPassword ||
+      !!editCredentialsFormHandler.errors.newPassword ||
+      !!editCredentialsFormHandler.errors.confirmPassword
+    );
+  };
+
+  const isInputInvalid = () => {
+    return (
+      noFieldsAreTouched() ||
+      (allFieldsAreTouched() && anyFieldInputIsInvalid())
+    );
+  };
+
   return (
     <ProfilePageContainerColumn>
-      {allData.map((data) => (
-        <ProfilePageContainerColumnItem key={data.name}>
-          <ProfilePageTypography fontSize="mdl">
-            {data.name}
-          </ProfilePageTypography>
-          <Input width="100%" />
+      <form onSubmit={onEditCredentialsHandler}>
+        <ProfilePageContainerColumnItem>
+          Current Password
+          <InputUserData
+            fontSizeDesktop="md"
+            width="100%"
+            type="password"
+            id="currentPassword"
+            value={editCredentialsFormHandler.values.currentPassword}
+            onChange={editCredentialsFormHandler.handleChange}
+            onBlur={editCredentialsFormHandler.handleBlur}
+          />
+          {editCredentialsFormHandler.errors.currentPassword ? (
+            <ErrorCard>
+              {editCredentialsFormHandler.errors.currentPassword}
+            </ErrorCard>
+          ) : null}
         </ProfilePageContainerColumnItem>
-      ))}
-      <Button>Change Password</Button>
+        <ProfilePageContainerColumnItem>
+          New Password
+          <InputUserData
+            fontSizeDesktop="md"
+            width="100%"
+            type="password"
+            id="newPassword"
+            value={editCredentialsFormHandler.values.newPassword}
+            onChange={editCredentialsFormHandler.handleChange}
+            onBlur={editCredentialsFormHandler.handleBlur}
+          />
+          {editCredentialsFormHandler.errors.newPassword ? (
+            <ErrorCard>
+              {editCredentialsFormHandler.errors.newPassword}
+            </ErrorCard>
+          ) : null}
+        </ProfilePageContainerColumnItem>
+        <ProfilePageContainerColumnItem>
+          Confirm Password
+          <InputUserData
+            fontSizeDesktop="md"
+            width="100%"
+            type="password"
+            id="confirmPassword"
+            value={editCredentialsFormHandler.values.confirmPassword}
+            onChange={editCredentialsFormHandler.handleChange}
+            onBlur={editCredentialsFormHandler.handleBlur}
+          />
+          {editCredentialsFormHandler.errors.confirmPassword ? (
+            <ErrorCard>
+              {editCredentialsFormHandler.errors.confirmPassword}
+            </ErrorCard>
+          ) : null}
+        </ProfilePageContainerColumnItem>
+        <Button type="submit" disabled={isInputInvalid()}>
+          Save changes
+        </Button>
+      </form>
     </ProfilePageContainerColumn>
   );
 };
